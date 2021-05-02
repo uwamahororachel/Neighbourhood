125ad95322a50ec382b09d73202709247a2109a2
Rachel Uwamahoro <racheluwamahoro55@gmail.com>
2021-05-02 10:27:05 +0200

    adding the url file

---
+++ hoodApp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+   
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
